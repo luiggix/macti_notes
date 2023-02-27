3b4e717540660124de573563ec006dfe99f29ca7
luiggix <luiggix@gmail.com>
2023-02-26 20:34:31 -0600

    2D

---
+++ notebooks/Derivada/DerivadasNumericas/Tools/hecompa_visual.py
@@ -0,0 +1,111 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import macti.visual as mvis
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+
+def set_axes(ax):
+    """
+    Configura la razón de aspecto, quita las marcas de los ejes y el marco.
+    
+    Parameters
+    ----------
+    ax: axis
+    Ejes que se vana configurar.
+    """
+    ax.set_aspect('equal') 
+    ax.set_xticks([])
+    ax.set_yticks([])
+    ax.spines['bottom'].set_visible(False)
+    ax.spines['left'].set_visible(False)
+    
+def plot_mesh(ax, xg, yg):
+    """
+    Dibuja la malla del dominio.
+    
+    Paramters
+    ---------
+    ax: axis
+    Son los ejes donde se dibujará la malla.
+    
+    xn: np.array
+    Coordenadas en x de la malla.
+    
+    yn: np.array
+    Coordenadas en y de la malla.
+    """
+    set_axes(ax)
+    
+    xn = xg[:,0]
+    yn = yg[0,:]
+    
+    for xi in xn:
+        ax.vlines(xi, ymin=yn[0], ymax=yn[-1], lw=0.5, color='darkgray')
+        
+    for yi in yn:
+        ax.hlines(yi, xmin=xn[0], xmax=xn[-1], lw=0.5, color='darkgray')
+        
+    ax.scatter(xg,yg, marker='.', color='darkgray')
+    
+def plot_frame(ax, xn, yn, lw = 0.5, color = 'k'):
+    """
+    Dibuja el recuadro de la malla.
+    
+    Paramters
+    ---------
+    ax: axis
+    Son los ejes donde se dibujará la malla.
+    
+    xn: np.array
+    Coordenadas en x de la malla.
+    
+    yn: np.array
+    Coordenadas en y de la malla.
+    """
+    set_axes(ax)
+    
+    # Dibujamos dos líneas verticales
+    ax.vlines(xn[0], ymin=yn[0], ymax=yn[-1], lw = lw, color=color)
+    ax.vlines(xn[-1], ymin=yn[0], ymax=yn[-1], lw = lw, color=color)
+
+    # Dibujamos dos líneas horizontales
+    ax.hlines(yn[0], xmin=xn[0], xmax=xn[-1], lw = lw, color=color)
+    ax.hlines(yn[-1], xmin=xn[0], xmax=xn[-1], lw = lw, color=color)
+    
+def set_canvas(ax, Lx, Ly):
+    """
+    Configura un lienzo para hacer las gráficas más estéticas.
+    
+    Parameters
+    ----------
+    ax: axis
+    Son los ejes que se van a configurar.
+    
+    Lx: float
+    Tamaño del dominio en dirección x.
+    
+    Ly: float
+    Tamaño del dominio en dirección y.
+    
+    Returns
+    -------
+    cax: axis
+    Eje donde se dibuja el mapa de color.
+    """
+    set_axes(ax)
+
+    lmax = max(Lx,Ly)
+    offx = lmax * 0.01
+    offy = lmax * 0.01
+    ax.set_xlim(-offx, Lx+offx)
+    ax.set_ylim(-offy, Ly+offy)
+    ax.grid(False)
+    
+    ax.set_aspect('equal')
+    divider = make_axes_locatable(ax)
+    cax = divider.append_axes("right", "5%", pad="3%")
+    cax.set_xticks([])
+    cax.set_yticks([])
+    cax.spines['bottom'].set_visible(False)
+    cax.spines['left'].set_visible(False)
+    
+    return cax
