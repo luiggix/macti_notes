4a118bb636826552125edef2a49ed6894501519e
luiggix <luiggix@gmail.com>
2023-11-08 15:19:56 +0000

    nuevo

---
+++ SMM_nbg/nbgrader_config.py
@@ -0,0 +1,6 @@
+c = get_config()
+
+c.CourseDirectory.course_id = "SMM_nbg"
+c.IncludeHeaderFooter.header = "source/header.ipynb"
+c.Exchange.root = "/usr/local/share/nbgrader/exchange"
+c.CourseDirectory.root = '/home/jovyan/SMM_nbg'
